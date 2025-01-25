9320b6ccff06059c8fe5ad51cf6888d22bc32a96
Yana Temirgalieva <kolesiko.yana@gmail.com>
2025-01-25 21:30:29 +0300

    feat: solve task 20

---
+++ tasks/20.py
@@ -0,0 +1,12 @@
+# Создать список и добавить в него 5 различных элементов.
+# Вывести содержимое списка на экран.
+
+elem_list = []
+
+elem_list.append('Hello to everybody!)')
+elem_list.append(1.2)
+elem_list.append(100)
+elem_list.append(True)
+elem_list.append(['a', 'b', 'c'])
+
+print(elem_list)
