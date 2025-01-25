9f024757ba38975708950ee5b5e0af9dd30a57bf
Yana Temirgalieva <kolesiko.yana@gmail.com>
2025-01-25 18:26:17 +0300

    feat: solve task 7

---
+++ tasks/07.py
@@ -0,0 +1,6 @@
+# Написать программу, которая принимает от пользователя целое число
+# и выводит на экран его квадрат.
+
+number = int(input('Inter an integer: '))
+square_of_num = number ** 2
+print('Square of number:', square_of_num)
