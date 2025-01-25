9cd5841f5aa7bf431d7bad355f4ca251eded3b26
Yana Temirgalieva <kolesiko.yana@gmail.com>
2025-01-25 20:03:45 +0300

    feat: solve task 14

---
+++ tasks/14.py
@@ -0,0 +1,6 @@
+# Создать множество, содержащее названия 5 разных городов,
+# затем удалить одно название и вывести оставшиеся.
+
+cities = {'Minsk', 'Sihanoukville', 'London', 'Tokyo', 'Madrid'}
+cities.remove('London')
+print(cities)
